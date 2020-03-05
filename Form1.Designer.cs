e63eb1e694e8375860d1c0c498eae97ddfe7b43f
Ice <your name>
2020-03-04 21:06:34 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Water_Bill_Analysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblThird = new System.Windows.Forms.Label();
+            this.lblFourth = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.txtFourthQuarter = new System.Windows.Forms.TextBox();
+            this.txtThirdQuarter = new System.Windows.Forms.TextBox();
+            this.txtSecondQuarter = new System.Windows.Forms.TextBox();
+            this.txtFirstQuarter = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.AutoSize = true;
+            this.lblFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirst.Location = new System.Drawing.Point(88, 50);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(88, 17);
+            this.lblFirst.TabIndex = 0;
+            this.lblFirst.Text = "First Quarter";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecond.Location = new System.Drawing.Point(88, 76);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(109, 17);
+            this.lblSecond.TabIndex = 1;
+            this.lblSecond.Text = "Second Quarter";
+            // 
+            // lblThird
+            // 
+            this.lblThird.AutoSize = true;
+            this.lblThird.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThird.Location = new System.Drawing.Point(88, 104);
+            this.lblThird.Name = "lblThird";
+            this.lblThird.Size = new System.Drawing.Size(94, 17);
+            this.lblThird.TabIndex = 2;
+            this.lblThird.Text = "Third Quarter";
+            // 
+            // lblFourth
+            // 
+            this.lblFourth.AutoSize = true;
+            this.lblFourth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFourth.Location = new System.Drawing.Point(95, 137);
+            this.lblFourth.Name = "lblFourth";
+            this.lblFourth.Size = new System.Drawing.Size(102, 17);
+            this.lblFourth.TabIndex = 3;
+            this.lblFourth.Text = "Fourth Quarter";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(464, 104);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(100, 18);
+            this.txtTotal.TabIndex = 4;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(464, 161);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(100, 18);
+            this.txtAverage.TabIndex = 5;
+            // 
+            // txtFourthQuarter
+            // 
+            this.txtFourthQuarter.Location = new System.Drawing.Point(221, 136);
+            this.txtFourthQuarter.Name = "txtFourthQuarter";
+            this.txtFourthQuarter.Size = new System.Drawing.Size(100, 18);
+            this.txtFourthQuarter.TabIndex = 6;
+            // 
+            // txtThirdQuarter
+            // 
+            this.txtThirdQuarter.Location = new System.Drawing.Point(221, 104);
+            this.txtThirdQuarter.Name = "txtThirdQuarter";
+            this.txtThirdQuarter.Size = new System.Drawing.Size(100, 18);
+            this.txtThirdQuarter.TabIndex = 7;
+            // 
+            // txtSecondQuarter
+            // 
+            this.txtSecondQuarter.Location = new System.Drawing.Point(221, 76);
+            this.txtSecondQuarter.Name = "txtSecondQuarter";
+            this.txtSecondQuarter.Size = new System.Drawing.Size(100, 18);
+            this.txtSecondQuarter.TabIndex = 8;
+            // 
+            // txtFirstQuarter
+            // 
+            this.txtFirstQuarter.Location = new System.Drawing.Point(221, 50);
+            this.txtFirstQuarter.Name = "txtFirstQuarter";
+            this.txtFirstQuarter.Size = new System.Drawing.Size(100, 18);
+            this.txtFirstQuarter.TabIndex = 9;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(402, 118);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(40, 17);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(402, 168);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(61, 17);
+            this.lblAverage.TabIndex = 11;
+            this.lblAverage.Text = "Average";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(193, 184);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 31);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(464, 218);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 27);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 416);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtFirstQuarter);
+            this.Controls.Add(this.txtSecondQuarter);
+            this.Controls.Add(this.txtThirdQuarter);
+            this.Controls.Add(this.txtFourthQuarter);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblFourth);
+            this.Controls.Add(this.lblThird);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblFirst);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Average Quarterly Bill";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblThird;
+        private System.Windows.Forms.Label lblFourth;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.TextBox txtFourthQuarter;
+        private System.Windows.Forms.TextBox txtThirdQuarter;
+        private System.Windows.Forms.TextBox txtSecondQuarter;
+        private System.Windows.Forms.TextBox txtFirstQuarter;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Water_Bill_Analysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+             //creating new veriable 
+            decimal FirstQuarter;
+            decimal SecondQuarter;
+            decimal ThirdQuarter;
+            decimal FourthQuarter;
+
+            if (decimal.TryParse(txtFirstQuarter.Text, out FirstQuarter))
+            {
+                // creating new list 
+                List<decimal> bill = new List<decimal>();
+                bill.Add(FirstQuarter); // Added new bill
+                MessageBox.Show(txtFirstQuarter.Text);
+
+            }
+            if (decimal.TryParse(txtSecondQuarter.Text, out SecondQuarter))
+            {
+                List<decimal> bill = new List<decimal>();
+                bill.Add(SecondQuarter);
+                MessageBox.Show(txtSecondQuarter.Text);
+            }
+            if (decimal.TryParse(txtThirdQuarter.Text, out ThirdQuarter))
+            {
+                List<decimal> bill = new List<decimal>();
+                bill.Add(ThirdQuarter);
+                MessageBox.Show(txtThirdQuarter.Text);
+            }
+            if (decimal.TryParse(txtFourthQuarter.Text, out FourthQuarter))
+            {
+                List<decimal> bill = new List<decimal>();
+                bill.Add(FourthQuarter);
+                MessageBox.Show(txtFourthQuarter.Text);
+            }
+            else
+            {
+                //You will have this message if you use letters
+
+                MessageBox.Show("please type numeric number!");
+                txtFirstQuarter.Clear();
+                txtSecondQuarter.Clear();
+                txtThirdQuarter.Clear();
+                txtFourthQuarter.Clear();
+            }
+
+            decimal total = FirstQuarter + SecondQuarter + ThirdQuarter + FourthQuarter; 
+            decimal averageBill = total / 4;
+            txtAverage.Text = averageBill.ToString(); //Average
+            txtTotal.Text = Convert.ToString(total);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
